13e312b61953b75154274d1ef2a02944cc22c32b
jhonat4n-hernandez <137846633+jhonat4n-hernandez@users.noreply.github.com>
2023-07-09 15:15:42 -0500

    PRIMER COMMIT
    SE SUBE EL PROYECTO

---
+++ Assets/botonpausa.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class botonpausa : MonoBehaviour
+{
+    public void pausa()
+    {
+        Time.timeScale = 0f;
+    }
+
+}
